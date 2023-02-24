d789d955663a9860471b45ace2d4d657317dddf2
Hulne <serge.hulne@gmail.com>
2023-02-24 10:21:28 +0100

    Update test_server.cr

--- src/test_server.cr
+++ src/test_server.cr
@@ -5,11 +5,8 @@ require "../archive.cr"
 HOST = "127.0.0.1"
 PORT = 8080
 
-
 # URL to archive path mapping
-def get_file_from_url?(url : String) : String
-  bk = Baker.new("public")
-  arc = get_archive()
+def get_file_from_url?(url : String, bk, arc) : String
   if url == "/" || url == ""
     url = "/index.html"
   end
@@ -23,10 +20,13 @@ end
 
 # Server using URL to archive path mapping
 server = HTTP::Server.new do |context|
-  context.response.print get_file_from_url?(context.request.path)
+  bk = Baker.new("public")
+  arc = get_archive()
+  context.response.print get_file_from_url?(context.request.path, bk, arc)
 end
 
 # Running server
 puts "Listening on http://#{HOST}:#{PORT}"
 server.bind_tcp HOST, PORT
 server.listen
+
